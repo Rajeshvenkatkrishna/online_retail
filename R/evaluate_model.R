ecb7a643e5a3f47d1093b282deae97143cb54adb
Rajeshvenkatkrishna <144311530+Rajeshvenkatkrishna@users.noreply.github.com>
2026-01-18 11:34:38 +0000

    R/evaluate_model.R

--- R/evaluate_model.R
+++ R/evaluate_model.R
@@ -1,13 +1,26 @@
 library(cluster)
 library(readr)
 
-data <- read_csv("data/processed/cleaned_online_retail.csv")
+data <- read_csv(
+  "data/processed/cleaned_online_retail.csv",
+  show_col_types = FALSE
+)
+
 scaled_data <- scale(data[, -1])
 
-km <- kmeans(scaled_data, centers = 4, nstart = 25)
+km <- kmeans(
+  scaled_data,
+  centers = 4,
+  nstart = 50,
+  iter.max = 100
+)
 
 sil <- silhouette(km$cluster, dist(scaled_data))
 avg_silhouette <- mean(sil[, 3])
+
+if (!dir.exists("outputs")) {
+  dir.create("outputs", recursive = TRUE)
+}
 
 writeLines(
   paste("Average Silhouette Score:", avg_silhouette),
